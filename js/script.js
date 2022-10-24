7d54c21839e203041748a0d04fa9089acf8c3ef1
cinvenz <vincenzobortune@gmail.com>
2022-10-24 17:46:46 +0200

    fix Dom html

--- js/script.js
+++ js/script.js
@@ -18,14 +18,14 @@ const eleSquares = document.querySelector('.squares');
 console.log('Numeri da 1 a 100');
 for (let i = 1; i <= 100; i++) { 
 	
-    if (i % 15 == 0){
-        eleSquares.innerHTML += `<div class="sq">${'fizzbuzz'}</div>`;
+    if (i % 3 == 0 && i % 5 != 0){
+        eleSquares.innerHTML += `<div class="sq3">${'fizz'}</div>`;
         console.log('fizz');
-    } else if (i % 3 == 0){
-        eleSquares.innerHTML += `<div class="sq">${'fizz'}</div>`;
+    } else if (i % 5 == 0 && i % 3 != 0){
+        eleSquares.innerHTML += `<div class="sq5">${'buzz'}</div>`;
         console.log('buzz');
-    } else if (i % 5 == 0) {
-        eleSquares.innerHTML += `<div class="sq>${'buzz'}</div>`;
+    } else if (i % 5 == 0 && i % 3 == 0) {
+        eleSquares.innerHTML += `<div class="sq15">${'fizzbuzz'}</div>`;
         console.log('fizzbuzz');
     }
     else {
