101227e1a2cd56d218f94f81b4ee758594214103
cinvenz <vincenzobortune@gmail.com>
2022-10-24 18:01:47 +0200

    add button generate

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 const eleSquares = document.querySelector('.squares');
+const eleBtn = document.querySelector('button')
 
-console.log('Numeri da 1 a 100');
-for (let i = 1; i <= 100; i++) { 
-	
-    if (i % 3 == 0 && i % 5 != 0){
-        eleSquares.innerHTML += `<div class="sq3">${'fizz'}</div>`;
-        console.log('fizz');
-    } else if (i % 5 == 0 && i % 3 != 0){
-        eleSquares.innerHTML += `<div class="sq5">${'buzz'}</div>`;
-        console.log('buzz');
-    } else if (i % 5 == 0 && i % 3 == 0) {
-        eleSquares.innerHTML += `<div class="sq15">${'fizzbuzz'}</div>`;
-        console.log('fizzbuzz');
+eleBtn.addEventListener('click', function () {
+    console.log('Numeri da 1 a 100');
+    for (let i = 1; i <= 100; i++) { 
+        
+        if (i % 3 == 0 && i % 5 != 0){
+            eleSquares.innerHTML += `<div class="sq3">${'fizz'}</div>`;
+            console.log('fizz');
+        } else if (i % 5 == 0 && i % 3 != 0){
+            eleSquares.innerHTML += `<div class="sq5">${'buzz'}</div>`;
+            console.log('buzz');
+        } else if (i % 5 == 0 && i % 3 == 0) {
+            eleSquares.innerHTML += `<div class="sq15">${'fizzbuzz'}</div>`;
+            console.log('fizzbuzz');
+        }
+        else {
+            console.log(i);
+            eleSquares.innerHTML += `<div class="sq">${i}</div>`;
+        }
+        
     }
-    else {
-        console.log(i);
-        eleSquares.innerHTML += `<div class="sq">${i}</div>`;
-    }
-    
-}
 
+});
