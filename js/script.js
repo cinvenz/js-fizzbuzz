5c6e611058915e812be3ad6a31705ec6c6327155
cinvenz <vincenzobortune@gmail.com>
2022-10-24 16:29:16 +0200

    aggiunto buzz ai numeri divisibili per 5 console

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ for (let i = 0; i <= 100; i++) {
 	console.log(i);
 
     if (i % 3){
-        console.log('Fizz');
+        console.log('fizz');
     }
 
     else if (i % 5){
-        
+        console.log('buzz');
     }
 }
 
